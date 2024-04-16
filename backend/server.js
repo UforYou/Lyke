2a43cbdf8ee136c1de3615e7bdd03639c75b1b9a
UforYou <shezanazmayor@gmail.com>
2024-04-16 23:31:47 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,58 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
-const app = express();
+ connectDB(); // Connect to MongoDB
+ const app = express();
 
-app.get('/', (req, res)=> {
-    res.send('API is runnng');
- });
+//BODY PARSER MIDDLEWARE
 
- app.get('/api/products',(req, res) => {
-    res.json(products);
-  } );
-
-  app.get('/api/products/:id', (req,res)=>{
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-  })
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
  
- app.listen (port,() => console.log(`Server is runninig on port ${port}`)); 
+
+
+ app.use('/api/products', productRoutes);
+ app.use('/api/users', userRoutes);
+ app.use('/api/orders', orderRoutes);
+ app.use('/api/upload', uploadRoutes);
+ 
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'/frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html')))
+
+
+} else {
+  app.get('/', (req, res,)=> {
+    res.send('API is runnng');
+ });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
